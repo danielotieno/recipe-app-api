84be2ff184522acf76ea6f78851b2b2925c80d5d
Daniel Otieno <danielotieno.ke@gmail.com>
2019-04-12 01:29:11 +0300

    Add doc string for the class

--- app/app/tests.py
+++ app/app/tests.py
@@ -1,8 +1,9 @@
 from django.test import TestCase
 from .calc import add, subtract
 
-class CalcTests(TestCase):
 
+class CalcTests(TestCase):
+    """Class for test cases"""
     def test_add_numbers(self):
         """Test that two numbers are added together"""
         self.assertEqual(add(10, 20), 30)
