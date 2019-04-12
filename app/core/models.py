fec8c588773df976908c5bd10727734297620c22
Daniel Otieno <danielotieno.ke@gmail.com>
2019-04-12 19:34:36 +0300

    Fix linting errors by flake8

--- app/core/models.py
+++ app/core/models.py
@@ -18,13 +18,22 @@ class UserManager(BaseUserManager):
 
         return user
 
+    def create_superuser(self, email, password):
+        """Creates and save a new superuser"""
+        user = self.create_user(email, password)
+        user.is_staff = True
+        user.is_superuser = True
+        user.save(using=self._db)
+
+        return user
+
 
 class User(AbstractBaseUser, PermissionsMixin):
     """Custom user model that supports email instead of username"""
-    email     = models.EmailField(max_length=255, unique=True)
-    name      = models.CharField(max_length=255)
+    email = models.EmailField(max_length=255, unique=True)
+    name = models.CharField(max_length=255)
     is_active = models.BooleanField(default=True)
-    is_staff  = models.BooleanField(default=False)
+    is_staff = models.BooleanField(default=False)
 
     objects = UserManager()
 
--- app/core/tests/test_models.py
+++ app/core/tests/test_models.py
@@ -9,13 +9,12 @@ class ModelTests(TestCase):
         email = 'daniel@gmail.com'
         password = 'Testpass1994'
         user = get_user_model().objects.create_user(
-            email = email,
-            password = password
-        )
+            email=email,
+            password=password
+            )
 
         self.assertEqual(user.email, email)
         self.assertTrue(user.check_password(password))
-
 
     def test_new_user_email_normalized(self):
         """Test convert the email address to lowercase"""
@@ -24,20 +23,17 @@ class ModelTests(TestCase):
 
         self.assertEqual(user.email, email.lower())
 
-
     def test_new_user_invalid_email(self):
         """Test creating user with no email raise error"""
         with self.assertRaises(ValueError):
             get_user_model().objects.create_user(None, 'Testpass1994')
-
 
     def test_create_new_super_user(self):
         """Test creating a new superuser"""
         user = get_user_model().objects.create_superuser(
             'daniel@gmail.com',
             'Testpass1994'
-        )
+            )
 
         self.assertTrue(user.is_superuser)
         self.assertTrue(user.is_staff)
-
