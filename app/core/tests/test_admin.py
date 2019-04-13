74253bf347828afd81a31502e2c7d336970b7b4a
Daniel Otieno <danielotieno.ke@gmail.com>
2019-04-13 20:09:20 +0300

    Create test change user page

--- app/core/tests/test_admin.py
+++ app/core/tests/test_admin.py
@@ -26,3 +26,10 @@ class AdminSiteTests(TestCase):
 
         self.assertContains(res, self.user.name)
         self.assertContains(res, self.user.email)
+
+    def test_user_change_page(self):
+        """Test that user edit page works correctly"""
+        url = reverse('admin:core_user_change', args=[self.user.id])
+        res = self.client.get(url)
+
+        self.assertEqual(res.status_code, 200)
