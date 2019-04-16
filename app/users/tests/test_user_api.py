bccb9ed334a91786035c27f8e364733c3baa3952
Daniel Otieno <danielotieno.ke@gmail.com>
2019-04-16 23:51:03 +0300

    Test that password is too short

--- app/users/tests/test_user_api.py
+++ app/users/tests/test_user_api.py
@@ -33,7 +33,7 @@ class PublicUserApiTests(TestCase):
 
     def test_user_exists(self):
         """Test creating a user that already exists fails"""
-        payload {
+        payload = {
             'email': 'test@gmail.com',
             'password': 'pass1234'
         }
@@ -43,3 +43,14 @@ class PublicUserApiTests(TestCase):
         res = self.client.post(CREATE_USER_URL, payload)
 
         self.assertEqual(res.status_code, status.HTTP_400_BAD_REQUEST)
+
+    def test_password_too_short(self):
+        """Test password must be more than 5 characters."""
+        payload = {'email': 'test@gmail.com', 'password': 'pass'}
+        res = self.client.post(CREATE_USER_URL, payload)
+
+        self.assertEqual(res.status_code, status.HTTP_400_BAD_REQUEST)
+        user_exists = get_user_model().objects.filter(
+            email=payload['email']
+        ).exists()
+        self.assertFalse(user_exists)
