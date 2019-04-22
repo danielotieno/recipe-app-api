96844e88fa2cd80a1f31782c2797011d4ec01d68
Daniel Otieno <danielotieno.ke@gmail.com>
2019-04-22 19:08:39 +0300

    Test tags are limited to user

--- app/recipe/tests/test_tag_api.py
+++ app/recipe/tests/test_tag_api.py
@@ -46,3 +46,18 @@ class PrivateTagsApiTest(TestCase):
 
         self.assertEqual(res.status_code, status.HTTP_200_OK)
         self.assertEqual(res.data, serializer.data)
+
+    def test_tags_limted_to_user(self):
+        """Test that tags return are for authenticated user."""
+        user2 = get_user_model().objects.create_user(
+            'user@gmail.com',
+            'userpassword'
+        )
+        Tag.objects.create(user=user2, name='Fruity')
+        tag = Tag.objects.create(user=self.user, name='Comfort Food')
+
+        res = self.client.get(TAGS_URL)
+
+        self.assertEqual(res.status_code, status.HTTP_200_OK)
+        self.assertEqual(len(res.data), 1)
+        self.assertEqual(res.data[0]['name'], tag.name)
