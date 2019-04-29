f664b11490ace857f679c1ed4b7d88220bc1d4f4
Daniel Otieno <danielotieno.ke@gmail.com>
2019-04-29 15:23:17 +0300

    Create sample recipe

---
+++ app/recipe/tests/test_recipe_api.py
@@ -0,0 +1,24 @@
+from django.contrib.auth import get_user_model
+from django.urls import reverse
+from django.test import TestCase
+
+from rest_framework import status
+from rest_framework.test import APIClient
+
+from core.models import Recipe
+
+from recipe.serializers import RecipeSerializer
+
+RECIPE_URL = reverse('recipe:recipe-list')
+
+
+def sample_recipe(user, **params):
+    """Create and return a sample recipe."""
+    defaults = {
+        'title': 'Sample Recipe',
+        'time_minutes': 10,
+        'price': 10.00
+    }
+    defaults.update(params)
+
+    return Recipe.objects.create(user=user, **defaults)
